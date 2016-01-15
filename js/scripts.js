dbfd96bedad21c5005ca0340c59fb19ceedc02af
brad london and nic netzel <london.brad@gmail.com>
2016-01-14 16:07:00 -0800

    make minor adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,19 @@ $(document).ready(function() {
         if (type === 'american'){
 
           var restaurant ="Interurban";
-          $(".restaurantSelection").text(restaurant);
         } else if (type === 'french'){
           var restaurant ="Petite Provence";
-          $(".restaurantSelection").text(restaurant);
         } else {
           var restaurant ="Enzo's Cafe Italian";
-          $(".restaurantSelection").text(restaurant);
         }
 
       } else {
         if (type === 'american'){
           var restaurant ="Holdfast Dining";
-          $(".restaurantSelection").text(restaurant);
         } else if (type === 'french'){
           var restaurant ="Le Pigeon";
-          $(".restaurantSelection").text(restaurant);
         } else {
           var restaurant ="Ava Gene's";
-          $(".restaurantSelection").text(restaurant);
         }
       }
 
@@ -39,34 +33,28 @@ $(document).ready(function() {
       if (price === 'cheap'){
         if (type === 'american'){
           var restaurant ="Tasty & Alder";
-          $(".restaurantSelection").text(restaurant);
         } else if (type === 'french'){
           var restaurant ="Le Happy";
-          $(".restaurantSelection").text(restaurant);
         } else {
           var restaurant ="Grassa";
-          $(".restaurantSelection").text(restaurant);
         }
 
       }else {
         if (type === 'american'){
           var restaurant ="Nomad PDX";
-          $(".restaurantSelection").text(restaurant);
         } else if (type === 'french'){
           var restaurant ="Paley's Place";
-          $(".restaurantSelection").text(restaurant);
         } else {
           var restaurant ="Mucca Osteria";
-          $(".restaurantSelection").text(restaurant);
-          $alert(restaurant);
+
         }
       }
     }
     $(".results").show();
-    alert(restaurant);
+    $(".restaurantSelection").text(restaurant);
 
     event.preventDefault();
-      console.log(restaurant)
+
   });
 
 
